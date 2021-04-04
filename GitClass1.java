10fcf226c75ab49104e15e405396b6e10280e66b
nilgun <n.cintav@gmail.com>
2021-04-04 23:01:32 +0300

    version 03

--- GitClass1.java
+++ GitClass1.java
@@ -4,5 +4,6 @@ public class GitClass1 {
 public static void main(String[] args) {
 	System.out.println("nihayet gitHub");
 	System.out.println("my second commit");
+	System.out.println("my third commit");
 }
 }
